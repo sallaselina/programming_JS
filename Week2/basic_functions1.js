5ccb307f7fbd199eb1836c401051837a70cc1915
Salla Närhi <salla.narhi97@gmail.com>
2024-01-19 14:05:30 +0200

    basic functions tasks done

---
+++ Week2/basic_functions1.js
@@ -0,0 +1,10 @@
+// Write the function sum such that it returns the sum of a and b.
+
+// sample usage - do not modify
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(1, 3));
+console.log(sum(2, 5));
---
+++ Week2/basic_functions2.js
@@ -0,0 +1,9 @@
+// Write the function multiply such that it returns the product (the result of multiplication) of a and b.
+
+// sample usage - do not modify
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(multiply(2, 4));
+console.log(multiply(3, 2));
