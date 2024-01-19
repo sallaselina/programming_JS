8f6029a0361d237d862b1c2c27153b5a982c93fb
Salla Närhi <salla.narhi97@gmail.com>
2024-01-19 13:11:59 +0200

    added operators task

---
+++ Week2/operators.js
@@ -0,0 +1,18 @@
+/* 
+- Create variables a and b, and assign them integer values.
+- Use add, subtraction, multiply, divide, and modulus operator for a and b. Store each result in separate variables (sum, difference, product, quotient, reminder).
+- Calculate the remainder of a divided by b using the modulus operator and store it in a variable called remainder.
+
+- Use console.log() to print the results of each operation to the console.
+
+- Add comments in your code to explain what each operation does. */
+
+let a = 14;
+let b = 66;
+let sum = a + b; // addition
+let difference = a - b; // subtraction
+let product = a * b; // multiplication
+let quotient = a / b; // division
+let remainder = a % b; //remainder of a divided by b
+
+console.log(sum, difference, product, quotient, remainder);
