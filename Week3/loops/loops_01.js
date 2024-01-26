cc8cbe00ec82be63257e9eec81dfe852b6d51f5f
Salla Närhi <salla.narhi97@gmail.com>
2024-01-26 14:55:13 +0200

    Add loops tasks 01 and 02

---
+++ Week3/loops/loops_01.js
@@ -0,0 +1,4 @@
+// Create a program that outputs all odd, positive numbers less than 100, starting from 1, such as 1, 3, 5, 7, 9, 11, and so on.
+for (let a = 1; a < 100; a += 2) {
+  console.log(a);
+}
